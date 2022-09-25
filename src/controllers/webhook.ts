eead217994d97591589db948b2136525646355c1
Andrej Zerox <kitanovicandrej213@gmail.com>
2022-09-25 20:53:37 +0200

    feat: google login save in webhook

--- src/controllers/webhook.ts
+++ src/controllers/webhook.ts
@@ -3,7 +3,7 @@ import { writeInFile } from 'helpers/writeInFile';
 
 export const postWebhookLog: RequestHandler = async (req, res, next) => {
 	try {
-		await writeInFile({ path: 'error.log', context: JSON.stringify(req.body) });
+		await writeInFile({ path: 'logs/request.log', context: JSON.stringify(req.body) });
 
 		res.json({
 			message: 'Logged',
